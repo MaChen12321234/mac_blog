dfe58421c573eaaf781bd1056ca934af617f9a70
machen <chen.ma@aliyun.com>
2025-05-08 01:33:52 +0800

    update blog_pub and blog_detail

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('blog/pub/', views.blog_pub, name='blog_pub'),
+    path('blog/detail/<int:bid>/', views.blog_detail, name='blog_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,19 @@
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, HttpResponse
 
 # Create your views here.
 
 
 def index(request):
-    print("push test")
+    """首页"""
     return render(request, 'index.html')
+
+
+def blog_pub(request):
+    """博客发布编辑"""
+    return render(request, 'pub-blog.html')
+
+
+def blog_detail(request, bid):
+    return render(request, 'blog_detail.html', {'bid': bid})
